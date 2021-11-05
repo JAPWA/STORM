43abb4a531312c7e655eb64fc0d3425518d03697
JAPWA <88171896+JAPWA@users.noreply.github.com>
2021-11-05 21:09:56 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -2892,6 +2892,22 @@ database:set(bot_id..'Num:Add:Bot',Num)
 send(msg.chat_id_, msg.id_,'  ✹︙ تم تعيين عدد الاعضاء سيتم تفعيل الجروبات التي اعضائها اكثر من  >> {'..Num..'} عضو')
 end
 
+if text == 'تحديث السورس' and DevSoFi(msg) then 
+if AddChannel(msg.sender_user_id_) == false then
+local textchuser = database:get(bot_id..'text:ch:user')
+if textchuser then
+send(msg.chat_id_, msg.id_,'['..textchuser..']')
+else
+send(msg.chat_id_, msg.id_,' ♫ لا تستطيع استخدام البوت \n ♫  يرجى الاشتراك بالقناه اولا \n ♫  اشترك هنا ['..database:get(bot_id..'add:ch:username')..']')
+end
+return false
+end
+os.execute('rm -rf DRAGON.lua')
+os.execute('wget https://raw.githubusercontent.com/#تيم_روبت/main/DRAGON.lua')
+send(msg.chat_id_, msg.id_,' ♫ تم تحديث السورس')
+dofile('DRAGON.lua')  
+end
+
 if text and text:match("^تغير الاشتراك$") and DevSoFi(msg) then  
 database:setex(bot_id.."add:ch:jm" .. msg.chat_id_ .. "" .. msg.sender_user_id_, 360, true)  
 send(msg.chat_id_, msg.id_, '  ✹︙ حسنآ ارسل لي معرف القناة')
@@ -15676,7 +15692,7 @@ Msᴀɢ ~ #msgs
 ]],
 [[
 🇪🇬≪💎≫ #username • メ
-🇪🇬≪💎≫ #stast  •メ
+🇪??≪💎≫ #stast  •メ
 🇪🇬≪💎≫ #id  • メ
 🇪🇬≪💎≫ #msgs  •メ
 🇪🇬≪💎≫ #game •メ
