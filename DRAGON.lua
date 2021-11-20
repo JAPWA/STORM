04e2de5c06bfbf447c00462d1683e7dbcffe1c01
JAPWA <88171896+JAPWA@users.noreply.github.com>
2021-11-20 15:06:32 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -17442,7 +17442,6 @@ keyboard = {}
 keyboard.inline_keyboard = {
 {{text = ' 𝒃𝒐𝒕', url="t.me/LC6BOT"}}, {text = ' 𝒃𝒐𝒕', url="t.me/DTeLebot"}}, 
 {{text = '°𝒔𝒐𝒖𝒓𝒄𝒆 𝒆𝒍𝒎𝒍𝒐𝒌‌‌‌°', url="t.me/eLmLoK0"}},
-}
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/LC6BOT&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
